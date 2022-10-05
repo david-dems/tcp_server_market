3286beaa1fa2eb1f14069facc96921bafa9336cf
david_dd <demuriya.ds@phystech.ru>
2022-10-05 23:43:02 +0300

    string_view in factory

--- include/client/EventProcessorFactory.h
+++ include/client/EventProcessorFactory.h
@@ -2,27 +2,28 @@
 
 #include <map>
 #include <string>
+#include <string_view>
 #include "ProcessorCreator.h"
 #include "Processor.h"
 
 class EventProcessorFactory{
 public:
-    Processor* makeProcessor(std::string processorName);
+    Processor* makeProcessor(std::string_view processorName);
     template <typename T>
-    void addProcessor(std::string processorName);
+    void addProcessor(std::string_view processorName);
     ~EventProcessorFactory();
 private:
-    std::map<std::string, ProcessorCreator*> creators_map;
+    std::map<std::string_view, ProcessorCreator*> creators_map;
 };
 
 template <typename T>
-void EventProcessorFactory::addProcessor(std::string processorName){
+void EventProcessorFactory::addProcessor(std::string_view processorName){
     if(creators_map.find(processorName) == creators_map.end())
         creators_map[processorName] = new TemplateProcessorCreator<T>;
 }
 
 
-Processor* EventProcessorFactory::makeProcessor(std::string processorName){
+Processor* EventProcessorFactory::makeProcessor(std::string_view processorName){
     if (creators_map.find(processorName) != creators_map.end())
         return creators_map[processorName]->create();
     return nullptr;
